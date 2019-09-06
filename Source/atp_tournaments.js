fd4ecc8720a455144821ff53f71631c556b72d57
Primosch <primosch@media-saturn.com>
2019-09-06 09:35:28 +0200

    last feature

--- Source/atp_tournaments.js
+++ Source/atp_tournaments.js
@@ -1,5 +1,7 @@
 const tournament = [
     [100001,"Brisbane","H",32,20190107,20190112,"Australia"],
     [100002,"Auckland","H",8,20190109,20190114,"Australia"],
-    [100003,"Sydney","H",64,20190109,20190114,"Australia"]
+    [100003,"Sydney","H",16,20190109,20190114,"Australia"],
+    [100003,"AustralianOpen","H",128,20190109,20190114,"Australia"],
+    [100003,"Madrid","H",64,20190109,20190114,"Australia"]
 ]
