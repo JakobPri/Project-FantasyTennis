0e0254c2187d47568bf43aab5d74cb8fca7b62cc
Primosch <primosch@media-saturn.com>
2019-09-17 12:58:26 +0200

    submitbutton reveals the result, new organisation of layout

--- FantasyTennis.js
+++ FantasyTennis.js
@@ -2,12 +2,15 @@ class FantasyTennis {
     constructor() {
         this.tournamentName = document.getElementById("chooseTournament").value;
         this.drawSize = 0;
+        this.pointCounter = 0
         this.guessedResultDraw = [];
         this.tournamentOptionButton = document.getElementById("chooseTournament")
-        this.pointCounter = 0;
         this.totalPoints = document.getElementById("totalPoint");
         this.submitButton = document.getElementById("submitButton");
         this.drawOverall = document.getElementById("DrawOverall")
+        this.playerGuesses = {}
+        this.playerButtons = document.getElementsByClassName("playerButton")
+
     }
     // This function adds for every available tournament one option to the Tournament Choosing Tab
     setUpTournament() {
@@ -49,7 +52,6 @@ class FantasyTennis {
         let playerButtons = Array.from(document.querySelectorAll(".playerButton"))
         for (let button of playerButtons) {
             button.onclick = () => {
-                this.pointCounter = this.pointCounter + 1
                 let gameOfButtonId = event.target.parentNode.id
                 let drawOfButtonId = event.target.parentNode.parentNode.id
 
@@ -57,63 +59,73 @@ class FantasyTennis {
                 let nextButtonId = (parseInt(gameOfButtonId.split("gameNr")[1]) & 1) ? "player1" : "player2"
                 let nextGameId = "gameNr" + (Math.ceil(gameOfButtonId.split("gameNr")[1] / 2))
                 document.querySelector("#" + nextDrawId + ">" + "#" + nextGameId + ">" + "#" + nextButtonId).innerText = event.target.innerText
+                this.playerGuesses[nextDrawId + "-" + nextGameId + "-" + nextButtonId] = event.target.innerText
+                this.pointCounter = this.pointCounter + Math.random()*4
 
-                if (Math.random() > 0.5) {
-                    document.querySelector("#" + nextDrawId + ">" + "#" + nextGameId + ">" + "#" + nextButtonId).style.background = "red";
-                } else {document.querySelector("#" + nextDrawId + ">" + "#" + nextGameId + ">" + "#" + nextButtonId).style.background = "green";
-            totalPoints = totalPoints + Math.random() * 8}
 
             }
         }
 
         this.submitButton.onclick = () => {
             SB.populateScoreBoard()
-
-
-        }
-        this.tournamentOptionButton.onchange = () => {
-            this.choosingTournament();
-            this.setupGraphic2(this.drawSize)
-            this.randomizeStartingPlayers();
-            this.clickEventHandlerSetup()
-        }
-
-    }
-
-    // This function adds the appropriate size to the draw and makes it visible
-    setupGraphic2(drawSize) {
-        for (let i = 1; i <= Math.log2(drawSize); i++) {
-            let drawDiv = document.createElement("div")
-            drawDiv.id = "draw-" + i
-            for (let j = 1; j <= Math.ceil(drawSize / (2 ** i)); j++) {
-                let gameBoxDiv = document.createElement("div");
-                gameBoxDiv.id = "gameNr" + j
-                gameBoxDiv.className = "game"
-                let player1BoxDiv = document.createElement("button");
-                player1BoxDiv.id = "player1"
-                player1BoxDiv.className = "playerButton"
-                let player2BoxDiv = document.createElement("button");
-                player2BoxDiv.id = "player2"
-                player2BoxDiv.className = "playerButton"
-                gameBoxDiv.appendChild(player1BoxDiv);
-                gameBoxDiv.appendChild(player2BoxDiv);
-                drawDiv.appendChild(gameBoxDiv);
-                this.drawOverall.appendChild(drawDiv)
+            console.log(this.playerButtons.length)
+            for (let i = 0; i < this.playerButtons.length-this.drawSize; i++) {
+                if (Math.random() > 0.5) {
+                    this.playerButtons[i + this.drawSize].style.background = "red";
+                } else {
+                    this.playerButtons[i + this.drawSize].style.background = "green";
+                    
+    
+                }
+                
             }
-        }
-        let winnerBoxDiv = document.createElement("div");
-        winnerBoxDiv.id = "draw-"+(Math.log2(drawSize)+1)
-        let winnerBoxdiv3 = document.createElement("div");
-        winnerBoxdiv3.id = "gameNr1"
-        winnerBoxdiv3.className = "game"
-        let winnerBoxButton2 = document.createElement("button");
-        winnerBoxButton2.id = "player1"
-        winnerBoxButton2.className = "playerButton"
-        winnerBoxdiv3.appendChild(winnerBoxButton2)
-        winnerBoxDiv.appendChild(winnerBoxdiv3);
-        this.drawOverall.appendChild(winnerBoxDiv)
 
+
+            }
+            this.tournamentOptionButton.onchange = () => {
+                this.choosingTournament();
+                this.setupGraphic2(this.drawSize)
+                this.randomizeStartingPlayers();
+                this.clickEventHandlerSetup()
+            }
+
+        }
+
+        // This function adds the appropriate size to the draw and makes it visible
+        setupGraphic2(drawSize) {
+            for (let i = 1; i <= Math.log2(drawSize); i++) {
+                let drawDiv = document.createElement("div")
+                drawDiv.id = "draw-" + i
+                for (let j = 1; j <= Math.ceil(drawSize / (2 ** i)); j++) {
+                    let gameBoxDiv = document.createElement("div");
+                    gameBoxDiv.id = "gameNr" + j
+                    gameBoxDiv.className = "game"
+                    let player1BoxDiv = document.createElement("button");
+                    player1BoxDiv.id = "player1"
+                    player1BoxDiv.className = "playerButton"
+
+                    let player2BoxDiv = document.createElement("button");
+                    player2BoxDiv.id = "player2"
+                    player2BoxDiv.className = "playerButton"
+                    gameBoxDiv.appendChild(player1BoxDiv);
+                    gameBoxDiv.appendChild(player2BoxDiv);
+                    drawDiv.appendChild(gameBoxDiv);
+                    this.drawOverall.appendChild(drawDiv)
+                }
+            }
+            let winnerBoxDiv = document.createElement("div");
+            winnerBoxDiv.id = "draw-" + (Math.log2(drawSize) + 1)
+            let winnerBoxdiv3 = document.createElement("div");
+            winnerBoxdiv3.id = "gameNr1"
+            winnerBoxdiv3.className = "game"
+            let winnerBoxButton2 = document.createElement("button");
+            winnerBoxButton2.id = "player1"
+            winnerBoxButton2.className = "playerButton"
+            winnerBoxdiv3.appendChild(winnerBoxButton2)
+            winnerBoxDiv.appendChild(winnerBoxdiv3);
+            this.drawOverall.appendChild(winnerBoxDiv)
+
+
+        }
 
     }
-
-}
